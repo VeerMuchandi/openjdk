c1c67986a80901cb7d35f67cbea94b93ff1c9ef4
VeerMuchandi <veer.muchandi@gmail.com>
2016-08-17 07:45:51 -0400

    changed init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 date
 java -version
-while true; do echo -e "HTTP/1.1 200 OK\n\n$(date)" | nc -ll -p 8080; done
+while true; do echo -e "HTTP/1.1 200 OK\n\n$(java -version)" | nc -ll -p 8080; done
